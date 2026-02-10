18673ce75866bd76005edbeed4ca5b666ef348a5
SITKA\michael.spelman <michael.spelman@sitkatech.com>
2026-02-10 12:46:59 -0800

    Changes required to get it to work

--- Nebula.EFModels/Entities/Generated/BackboneSegmentType.cs
+++ Nebula.EFModels/Entities/Generated/BackboneSegmentType.cs
@@ -4,30 +4,26 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.EntityFrameworkCore;
 
-namespace Nebula.EFModels.Entities
+namespace Nebula.EFModels.Entities;
+
+[Table("BackboneSegmentType")]
+[Index("BackboneSegmentTypeDisplayName", Name = "AK_BackboneSegmentType_BackboneSegmentTypeDisplayName", IsUnique = true)]
+[Index("BackboneSegmentTypeName", Name = "AK_BackboneSegmentType_BackboneSegmentTypeName", IsUnique = true)]
+public partial class BackboneSegmentType
 {
-    [Table("BackboneSegmentType")]
-    [Index("BackboneSegmentTypeDisplayName", Name = "AK_BackboneSegmentType_BackboneSegmentTypeDisplayName", IsUnique = true)]
-    [Index("BackboneSegmentTypeName", Name = "AK_BackboneSegmentType_BackboneSegmentTypeName", IsUnique = true)]
-    public partial class BackboneSegmentType
-    {
-        public BackboneSegmentType()
-        {
-            BackboneSegments = new HashSet<BackboneSegment>();
-        }
+    [Key]
+    public int BackboneSegmentTypeID { get; set; }
 
-        [Key]
-        public int BackboneSegmentTypeID { get; set; }
-        [Required]
-        [StringLength(20)]
-        [Unicode(false)]
-        public string BackboneSegmentTypeName { get; set; }
-        [Required]
-        [StringLength(20)]
-        [Unicode(false)]
-        public string BackboneSegmentTypeDisplayName { get; set; }
+    [Required]
+    [StringLength(20)]
+    [Unicode(false)]
+    public string BackboneSegmentTypeName { get; set; }
 
-        [InverseProperty("BackboneSegmentType")]
-        public virtual ICollection<BackboneSegment> BackboneSegments { get; set; }
-    }
+    [Required]
+    [StringLength(20)]
+    [Unicode(false)]
+    public string BackboneSegmentTypeDisplayName { get; set; }
+
+    [InverseProperty("BackboneSegmentType")]
+    public virtual ICollection<BackboneSegment> BackboneSegments { get; set; } = new List<BackboneSegment>();
 }
--- Nebula.EFModels/Entities/Generated/vGeoServerWatershed.cs
+++ Nebula.EFModels/Entities/Generated/vGeoServerWatershed.cs
@@ -5,19 +5,21 @@ using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.EntityFrameworkCore;
 using NetTopologySuite.Geometries;
 
-namespace Nebula.EFModels.Entities
+namespace Nebula.EFModels.Entities;
+
+[Keyless]
+public partial class vGeoServerWatershed
 {
-    [Keyless]
-    public partial class vGeoServerWatershed
-    {
-        public int PrimaryKey { get; set; }
-        public int WatershedID { get; set; }
-        [Required]
-        [StringLength(100)]
-        [Unicode(false)]
-        public string WatershedName { get; set; }
-        [Required]
-        [Column(TypeName = "geometry")]
-        public Geometry WatershedGeometry4326 { get; set; }
-    }
+    public int PrimaryKey { get; set; }
+
+    public int WatershedID { get; set; }
+
+    [Required]
+    [StringLength(100)]
+    [Unicode(false)]
+    public string WatershedName { get; set; }
+
+    [Required]
+    [Column(TypeName = "geometry")]
+    public Geometry WatershedGeometry4326 { get; set; }
 }
