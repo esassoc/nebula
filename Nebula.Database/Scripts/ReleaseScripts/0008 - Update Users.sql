736d34517e8620f03039cbddb21aa1b1fc6dd2c4
SITKA\michael.spelman <michael.spelman@sitkatech.com>
2026-03-03 12:23:52 -0800

    Updates to users

--- Nebula.Database/Scripts/ReleaseScripts/0008 - Update Users.sql
+++ Nebula.Database/Scripts/ReleaseScripts/0008 - Update Users.sql
@@ -12,8 +12,16 @@ BEGIN
     UPDATE dbo.[User] SET Email = 'JBurns@esassoc.com' WHERE Email = 'john.burns@sitkatech.com';  -- Replace active sitkatech users with esassoc
 	UPDATE dbo.[User] SET Email = 'KElmquist@esassoc.com' WHERE Email = 'kathleen.elmquist@sitkatech.com';
 	UPDATE dbo.[User] SET Email = 'MPeters@esassoc.com' WHERE Email = 'mack.peters@sitkatech.com';
-	UPDATE dbo.[User] SET Email = 'MFerrante@esassoc.com' WHERE Email = 'michael@sitkatech.com';
     UPDATE dbo.[User] SET Email = 'RLee@esassoc.com' WHERE Email = 'ray@sitkatech.com';
+
+    -- His Nebula user record is tied to Keith's Keystone guid...
+    UPDATE dbo.[User] SET LegacyUserGuid = '76addc70-2ee5-438a-a7ad-17876f76ec30' WHERE Email = 'sgordon@esassoc.com';
+
+    -- She has two Keystone accounts, one for sitkatech and one for esassoc.  However, the esassoc account was tied to an invalid email... cannot migrate
+    UPDATE dbo.[User] SET RoleID = 4 WHERE Email = 'jquishenberry@esassoc.com';
+
+    -- I am trying to scrub out all sitkatech emails, but his keystone account still uses sitkatech, so he gets deactivated and is not migrated
+    UPDATE dbo.[User] SET Email = 'MFerrante@esassoc.com', RoleID = 4 WHERE Email = 'michael@sitkatech.com';
 
     INSERT INTO dbo.[User](LegacyUserGuid, FirstName, LastName, Email, RoleID, CreateDate, IsActive, ReceiveSupportEmails, LoginName)
     SELECT 'b374c4b7-f83a-475f-88d0-ec270220da8e', 'Michael', 'Spelman', 'mspelman@esassoc.com', 1, GetDate(), 1, 0, 'mspelman'
